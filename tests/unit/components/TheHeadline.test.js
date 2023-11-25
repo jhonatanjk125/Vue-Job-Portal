d0e9664713bed00546f1b9985f6e43292bd70167
jhonatanjk125 <jhonatanjk125@gmail.com>
2023-11-25 16:51:44 -0500

    Add additional tests to headline component

--- tests/unit/components/TheHeadline.test.js
+++ tests/unit/components/TheHeadline.test.js
@@ -1,24 +1,46 @@
 import { render, screen } from '@testing-library/vue'
+import { nextTick } from 'vue'
 
 import TheHeadline from '@/components/TheHeadline.vue'
 
 describe('TheHeadline', () => {
-  it('displays introductory action verbs', () => {
+  beforeEach(() => {
     vi.useFakeTimers()
+  })
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+  it('displays introductory action verb', () => {
     render(TheHeadline)
     const actionPhrase = screen.getByRole('heading', {
       name: /build for everyone/i
     });
     expect(actionPhrase).toBeInTheDocument();
-    vi.useRealTimers();
     
     it('changes action verb at a consistent interval', () => {
-      vi.useFakeTimers()
       const mock = vi.fn()
       vi.stubGlobal('setInterval', mock)
       render(TheHeadline)
       expect(mock).toHaveBeenCalled();
-      vi.useRealTimers();
     })
+  })
+  it('swaps action verb after interval', async () => {
+    render(TheHeadline);
+    vi.advanceTimersToNextTimer();
+    await nextTick();
+    const actionPhrase = screen.getByRole('heading', {
+      name: /create for everyone/i
+    });
+
+    expect(actionPhrase).toBeInTheDocument();
+  })
+
+  it('removes interval when component is unmounted', () => {
+    const clearInterval = vi.fn();
+    vi.stubGlobal('clearInterval', clearInterval);
+    const { unmount } = render(TheHeadline);
+    unmount();
+    expect(clearInterval).toHaveBeenCalled();
+    vi.unstubAllGlobals();
   })
 })
